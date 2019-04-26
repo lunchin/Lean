c0056d1a28a6c4076f1f575285013e266ba5c02c
Martin Molinero <martin.molinero1@gmail.com>
2019-04-26 16:35:24 -0300

    Add ConstituentsUniverseDataGenerator algorithm
    - Fix `ToString` implementation of the `SecurityIdentifier.None`. Adding
    unit test

--- Common/SecurityIdentifier.cs
+++ Common/SecurityIdentifier.cs
@@ -638,7 +638,7 @@ namespace QuantConnect
         {
             exception = null;
 
-            if (string.IsNullOrWhiteSpace(value))
+            if (string.IsNullOrWhiteSpace(value) || value == " 0")
             {
                 identifier = Empty;
                 return true;
@@ -768,6 +768,7 @@ namespace QuantConnect
         public override string ToString()
         {
             var props = EncodeBase36(_properties);
+            props = props.IsNullOrEmpty() ? "0" : props;
             if (HasUnderlying)
             {
                 return _symbol + ' ' + props + '|' + _underlying.SecurityIdentifier;
